c0c7302cc7431cc44cb47518c4f44506b76485d9
Jensone <hello@jensone.com>
2025-02-06 06:47:05 +0100

    Update user profile form handling to include submission logic and flash messaging for successful updates

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,26 +2,39 @@
 
 namespace App\Controller;
 
+use App\Form\UserFormType;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class UserController extends AbstractController
 {
     #[Route('/profile', name: 'app_profile', methods: ['GET', 'POST'])]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $em): Response
     {
+        $user = $this->getUser();
+        $form = $this->createForm(UserFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($user);
+            $em->flush();
+
+            // Redirection avec flash message
+            $this->addFlash('success', 'Votre profil à été mis à jour');
+            return $this->redirectToRoute('app_profile');
+        }
         return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+            'userForm' => $form,
         ]);
     }
 
     #[Route('/complete', name: 'app_complete', methods: ['POST'])]
     public function complete(Request $request, EntityManagerInterface $em): Response
     {
-        $data = $request->getPayload();
+        $data = $request->getPayload(); // on récupère les données du formulaire
         if (!empty($data->get('username')) && !empty($data->get('fullname'))) {
             // Enregistrer les données dans la base de données
             $user = $this->getUser(); // Ici on récupère l'utilisateur actuel
